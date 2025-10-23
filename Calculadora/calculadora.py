c593f2a0677757dcc0fb9ce60d592c07407e6854
Andrey-XP <andrey.sousalipe@gmail.com>
2025-10-23 01:01:06 -0300

    Mudei o codigo pra calcular

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -1,1 +1,21 @@
-N1 = int(input('Digite um numero'))
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Outro Numero: '))
+operacao = input('Escolha a operação : (+, -, *, /)')
+
+if operacao == '+':
+    resultado = n1 + n2
+    print(f'{n1} + {n2} = {resultado}')
+elif operacao == '-':
+    resultado = n1 - n2
+    print(f'{n1} - {n2} = {resultado}')
+elif operacao == '*':
+    resultado = n1 * n2
+    print(f'{n1} * {n2} = {resultado}')
+elif operacao == '/':
+    if n2 == 0:
+        print('Erro: divisão por zero.')
+    else:
+        resultado = n1 / n2
+        print(f'{n1} / {n2} = {resultado}')
+else:
+    print('Operação inválida. Escolha entre +, -, * ou /.')
